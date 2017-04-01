f223556eb1bfdd30e640dd5114d28051bee53dd0
vcordero07 <vcordero07@gmail.com>
2017-03-31 22:04:04 -0400

    Improved code for the catCarouselApp

--- catCarousel/catCarouselApp.js
+++ catCarousel/catCarouselApp.js
@@ -1,3 +1,7 @@
+// Cat carousel
+// In this challenge, you need to write JavaScript that provides the following
+// experience: when a user clicks on one of the thumbnail images, that image
+// should be displayed in the full size image container at the top.
 // your code here
 
 let catImgToggle = () => {
@@ -8,6 +12,33 @@ let catImgToggle = () => {
         $('.hero img').attr('src', catSrc);
     });
 
+};
+
+$(catImgToggle());
+
+
+// your code here
+let catImgToggle = () => {
+    $('.thumbnail').on('click', 'img', function(event) {
+        //alert('it works');
+        console.log($(this).attr('src'));
+        let catSrc = $(this).attr('src');
+        $('.hero img').attr('src', catSrc);
+    });
+};
+
+$(catImgToggle());
+
+
+// your code here
+let catImgToggle = () => {
+    $('.thumbnail').on('click', 'img', (event) => {
+        //alert('it works');
+        //console.log($(this).attr('src'));
+        console.log($(event.currentTarget).attr('src'));
+        let catSrc = $(event.currentTarget).attr('src');
+        $('.hero img').attr('src', catSrc);
+    });
 };
 
 $(catImgToggle());
