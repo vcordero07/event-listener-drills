1e9cb1836473ffa890da8691a344e0ccc593573c
vcordero07 <vcordero07@gmail.com>
2017-03-31 23:37:19 -0400

    Improved code for the lightBulbToggleApp

--- lightBulbtoggle/lightBulbToggleApp.js
+++ lightBulbtoggle/lightBulbToggleApp.js
@@ -1,3 +1,12 @@
+// Lightbulb Toggle
+// In this drill, you'll need to use event listeners to detect when users click
+// on an element with the .js-lightbulb class. When that happens, the current
+// clicked element should get the class .bulb-on applied to it, and any other
+// elements that previously had the .bulb-on class should have it removed.
+//
+// With the CSS styles already in place, this will cause the currently clicked
+// lightbulb to appear to turn on, and any other lit bulbs off.
+
 let handleBulbClicks = () => {
     $('.lightbulb').click(function(event) {
         $('.lightbulb').removeClass('bulb-on');
@@ -6,3 +15,18 @@ let handleBulbClicks = () => {
 };
 
 $(handleBulbClicks());
+
+
+
+//----------------------
+// your code here
+
+let lightBulbToggle = () => {
+    $('.lightbulb').click(function(event) {
+        $('.lightbulb').removeClass('bulb-on');
+        //console.log( $(this).addClass('bulb-on'));
+        $(this).addClass('bulb-on')
+    });
+};
+
+$(lightBulbToggle());
